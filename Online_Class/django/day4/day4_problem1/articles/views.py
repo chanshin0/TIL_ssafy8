2435749655c2adc8986371c4414c4f21224c4a1d
dongchan <windcj1@naver.com>
2022-09-14 01:04:00 +0900

    0914 TIL

--- Online_Class/django/day4/day4_problem1/articles/views.py
+++ Online_Class/django/day4/day4_problem1/articles/views.py
@@ -36,3 +36,18 @@ def delete(request, pk):
     article = Article.objects.get(pk=pk)
     article.delete()
     return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article,
+    }
+    return render(request, 'edit.html', context)
+    
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
+    
